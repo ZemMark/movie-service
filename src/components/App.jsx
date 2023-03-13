81205102db87e6fa093cf13e4851c8fc6d3fdef8
Mark Zemlianskyi <mark080501@gmail.com>
2023-03-13 17:41:08 +0200

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { SearchForm } from './search/Search'
+
+function App() {
+
+  return(
+    <div>
+      <h1>I'm am started and you can not stop me!</h1>
+      <SearchForm></SearchForm>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/search/Search.jsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+export function SearchForm() {
+  // return <Button color="secondary">Secondary</Button>;
+  return (
+    <Button variant="contained" color="primary">
+  Click me!
+</Button>
+  )
+}
